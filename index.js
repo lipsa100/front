9f16360649ee859a451be44219224818f0f75ec8
lipsa100 <73013154+lipsa100@users.noreply.github.com>
2021-05-26 13:22:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// var audio=new Audio("cmyn.mp3");
+// var button = document.querySelector(".play"),count = 0;
+// document.getElementByTag('body').style.backgroundImage = "url('background.gif?v=" + new Date().valueOf() + "')"
+
+// button.onclick = function() {
+//   count += 1;
+
+// if (count % 2 ==0){
+//   document.querySelector(".play").innerHTML="▶";
+//  audio.pause();
+// }
+// else{
+//   document.querySelector(".play").innerHTML="≈";
+// audio.play();
+//   }
+// };
+
+// setTimeout(function() {
+// document.getElementsByTagName('body').add('video');
+// }, 5000);
+
+// setTimeout(function() {
+//   document.getElementsByTagName('body').remove('video');
+//  }, 10000);
